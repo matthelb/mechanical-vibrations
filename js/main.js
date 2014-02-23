aec6d82a5907a840636edf18708a71029d9c8e39
Matthew Burke <matthelb@usc.edu>
2014-02-23 15:22:49 -0800

    First commit to pages

---
+++ js/main.js
@@ -0,0 +1,66 @@
+$(document).ready(function(){
+	$('#simulate-button').click(function(){
+		var m = parseFloat($('#m-input').val());
+		var k = parseFloat($('#k-input').val());
+		var y0 = parseFloat($('#y0-input').val());
+		var v0 = parseFloat($('#v0-input').val());
+		var frames = parseInt($('#time-input').val());
+		var start = parseFloat($('#start-input').val());
+		var end = parseFloat($('#end-input').val());
+		var spring = new Spring(m, k, y0, v0, UndampedFreeVibration);
+		graph(start, end, spring, frames);
+
+	});
+});
+
+function graph(start, end, spring, seconds) {
+	var series = [];
+	var container = $('#placeholder');
+	var plot = $.plot(container, [series], {
+		grid: {
+			borderWidth: 1,
+			minBorderMargin: 20,
+			labelMargin: 10,
+			backgroundColor: {
+				colors: ["#fff", "#e4f4f4"]
+			},
+			margin: {
+				top: 8,
+				bottom: 20,
+				left: 20
+			},
+			markings: function(axes) {
+				var markings = [];
+				var xaxis = axes.xaxis;
+				for (var x = Math.floor(xaxis.min); x < xaxis.max; x += xaxis.tickSize * 2) {
+					markings.push({ xaxis: { from: x, to: x + xaxis.tickSize }, color: "rgba(232, 232, 255, 0.2)" });
+				}
+				return markings;
+			}
+		},
+		xaxis: {
+			min: start,
+			max: end
+		},
+		yaxis: {
+			min: -Math.ceil(spring.type.getAmplitude() * 1.1),
+			max: Math.ceil(spring.type.getAmplitude() * 1.1)
+		},
+		legend: {
+			show: true
+		}
+	});
+
+	var t = start;
+	var i = (end-start)/(seconds * 60);
+	setInterval(function(){
+		if(t > end) 
+			return;
+		series.push([t, spring.type.call(t)]);
+		t += i;
+		plot.setData([series]);
+		plot.setupGrid();
+		plot.draw();
+		spring.draw(t, $('#spring-canvas')[0]);
+	}, 16);
+}
---
+++ js/mechanical-vibrations.js
@@ -0,0 +1,46 @@
+function Spring(m, k, y0, v0, type) {
+	this.type = new type(m, k, y0, v0);
+	this.y = this.type.call;
+}
+
+Spring.prototype.draw = function(t, canvas) {
+	var context = canvas.getContext('2d');
+	context.fillStyle = "blue";
+	context.clearRect(0, 0, canvas.width, canvas.height)
+	var y = this.type.getAmplitude() - this.type.call(t);
+	y = (canvas.height / 4) * y / this.type.getAmplitude();
+	var size = Math.max(10, Math.min(100, this.type.m));
+	var n = 10;
+	var i = y / n;
+	var x = size / 2;
+	drawZigZag(context, x, 0, n, i, 8)
+	drawZigZag(context, x, 0, n, i, -8)
+	context.fillRect(0, y, size, size);
+}
+
+function drawZigZag(context, x, y, n, spacing, width) {
+	context.beginPath();
+	var o = width / 2;
+	context.moveTo(x - o, 0);
+	for (var j = 1; j <= n; j++) {
+		context.lineTo(x + o, j * spacing);
+		o = -o;
+	}
+	context.stroke();
+	context.closePath();
+}
+function UndampedFreeVibration(m, k, y0, v0) {
+	this.m = m;
+	this.k = k;
+	this.y0 = y0;
+	this.v0 = v0;
+	this.w0 = Math.sqrt(k / m);
+}
+
+UndampedFreeVibration.prototype.call = function(t) {
+	return this.y0 * Math.cos(this.w0 * t) + this.v0 / this.w0 * Math.sin(this.w0 * t);
+}
+
+UndampedFreeVibration.prototype.getAmplitude = function() {
+	return Math.max(Math.abs(this.y0), Math.abs(this.v0 / this.w0));
+}
